d82c6ac11db122ae6369a21deb4414e04368ef93
hvzzzz <hananquispec@outlook.com>
2026-04-04 17:26:27 +0200

    initial working fpu test

---
+++ verif/regress/fpu-tests-cv64a6_imafdc_sv39.sh
@@ -0,0 +1,53 @@
+# Copyright 2021 Thales DIS design services SAS
+# Inria 2026
+#
+# Licensed under the Solderpad Hardware Licence, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# SPDX-License-Identifier: Apache-2.0 WITH SHL-2.0
+# You may obtain a copy of the License at https://solderpad.org/licenses/
+#
+# Original Author: Jean-Roch COULON - Thales
+# Modified by Nicolas Derumigny - Inria
+
+# where are the tools
+if ! [ -n "$RISCV" ]; then
+  echo "Error: RISCV variable undefined"
+  return
+fi
+
+#if ! [ -n "$DV_SIMULATORS" ]; then
+# Only tested (working?) with Verilator
+  DV_SIMULATORS=veri-testharness
+#fi
+
+# install the required tools
+if [[ "$DV_SIMULATORS" == *"veri-testharness"* ]]; then
+  source ./verif/regress/install-verilator.sh
+fi
+source ./verif/regress/install-spike.sh
+
+# install the required test suites
+source ./verif/regress/install-riscv-compliance.sh
+source ./verif/regress/install-riscv-tests.sh
+source ./verif/regress/install-riscv-arch-test.sh
+
+# setup sim env
+source ./verif/sim/setup-env.sh
+
+echo "$SPIKE_INSTALL_DIR$"
+
+if ! [ -n "$UVM_VERBOSITY" ]; then
+    export UVM_VERBOSITY=UVM_NONE
+fi
+
+export DV_OPTS="$DV_OPTS --issrun_opts=+debug_disable=1+UVM_VERBOSITY=$UVM_VERBOSITY"
+
+CC_OPTS="-static -mcmodel=medany -fvisibility=hidden -nostdlib -nostartfiles -g ../tests/multicore/common/syscalls.c ../tests/multicore/common/crt.S -I../tests/multicore/env -I../tests/multicore/common -lgcc"
+
+
+cd verif/sim/
+
+python3 cva6.py --nr_harts 2 --c_tests ../tests/multicore/fpu_share/fpu_test.c --output_ref_file=../tests/multicore/references/fpu_test --iss_yaml cva6.yaml --target cv64a6_imafdc_sv39 --iss=$DV_SIMULATORS --gcc_opts="$CC_OPTS" $DV_OPTS --linker=../../config/gen_from_riscv_config/linker/link.ld 3>&1 1>&2 2>&3 | colout -t cva6 3>&1 1>&2 2>&3
+# python3 cva6.py --nr_harts 2 --c_tests ../tests/multicore/lr_sc/lr_sc.c  --output_ref_file=../tests/multicore/references/lr_sc --iss_yaml cva6.yaml --target cv64a6_imafdc_sv39 --iss=$DV_SIMULATORS --gcc_opts="$CC_OPTS -nostdlib -lgcc" $DV_OPTS --linker=../../config/gen_from_riscv_config/linker/link.ld 3>&1 1>&2 2>&3 | colout -t cva6 3>&1 1>&2 2>&3
+
+cd -
---
+++ verif/tests/multicore/fpu_share/fpu_test.c
@@ -0,0 +1,54 @@
+/*
+**
+** Copyright 2020 OpenHW Group
+**
+** Licensed under the Solderpad Hardware Licence, Version 2.0 (the "License");
+** you may not use this file except in compliance with the License.
+** You may obtain a copy of the License at
+**
+**     https://solderpad.org/licenses/
+**
+** Unless required by applicable law or agreed to in writing, software
+** distributed under the License is distributed on an "AS IS" BASIS,
+** WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+** See the License for the specific language governing permissions and
+** limitations under the License.
+**
+*/
+
+#include <stdint.h>
+#include <stdio.h>
+
+#include "util.h"
+
+int main(int argc, char *arg[]) {
+  float op1 = 1.4142;
+  float op2 = 1.4142;
+  float ans = op1 * op2;
+
+  // Cast the memory address of the floats to uint32_t pointers
+  uint32_t op1_bits = *(uint32_t *)&op1;
+  uint32_t op2_bits = *(uint32_t *)&op2;
+  uint32_t ans_bits = *(uint32_t *)&ans;
+
+  /* printf("op1 in core %d: 0x%08x\n", get_hart_id(), op1_bits); */
+  /* printf("op2 in core %d: 0x%08x\n", get_hart_id(), op2_bits); */
+  printf("result in core %d: 0x%08x\n", get_hart_id(), ans_bits);
+  /* uint32_t core_id = get_hart_id(); */
+  /* if (core_id == 0) { */
+  /*   float a = 3.14; */
+  /*   float b = 2.0; */
+  /*   float res_add = a + b; */
+  /*   uint32_t bits = *(uint32_t *)&res_add; */
+  /*   printf("Core 0 Addition: 0x%08x\n", bits); */
+
+  /* } else if (core_id == 1) { */
+  /*   float x = 1.4142; */
+  /*   float y = 1.4142; */
+  /*   float res_mul = x * y; */
+  /*   uint32_t bits = *(uint32_t *)&res_mul; */
+  /*   printf("Core 1 Multiplication: 0x%08x\n", bits); */
+  /* } */
+
+  return 0;
+}

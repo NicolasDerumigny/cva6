b050da81ece44e4aaa27c888e7a2d72ba595e7bd
hvzzzz <hananquispec@outlook.com>
2026-04-05 12:46:45 +0200

    exposed fpu_rm_id_ex

--- core/cva6_multicore.sv
+++ core/cva6_multicore.sv
@@ -271,6 +271,8 @@ module cva6_multicore
   // fpu fmt signal from issue
   logic [1:0] fpu_fmt_iss_fpu_i;
 
+  // fpu rm signal from issue
+  logic [2:0] fpu_rm_iss_fpu_i;
   // Cores + (private) I$
   generate
     for (genvar HartId = 0; HartId < NrHarts; HartId++) begin : gen_one_core
@@ -346,7 +348,8 @@ module cva6_multicore
           .fu_data_iss_2_fpu_o           (fu_data_iss_2_fpu_i),
           .flush_ctrl_fpu_o              (flush_ctrl_fpu_i),
           .fpu_valid_o                   (fpu_valid_iss_2_fpu_i),
-          .fpu_fmt_iss_fpu_o             (fpu_fmt_iss_fpu_i)
+          .fpu_fmt_iss_fpu_o             (fpu_fmt_iss_fpu_i),
+          .fpu_rm_iss_fpu_o              (fpu_rm_iss_fpu_i)
       );
 
       cva6_icache #(

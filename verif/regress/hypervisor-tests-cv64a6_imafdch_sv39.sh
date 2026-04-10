5479b4f0a838d08c15a8a0f35c08e1dc897dd9f6
Nicolas Derumigny <nderumigny@gmail.com>
2026-04-10 18:13:20 +0200

    fix(hpdcache): correct lr/sc

--- verif/regress/hypervisor-tests-cv64a6_imafdch_sv39.sh
+++ verif/regress/hypervisor-tests-cv64a6_imafdch_sv39.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 # Copyright 2021 Thales DIS design services SAS
 # Inria 2026
 #
--- verif/tests/multicore/lr_sc/lr_sc.c
+++ verif/tests/multicore/lr_sc/lr_sc.c
@@ -14,12 +14,66 @@ static inline int atomic_inc(int *addr) {
         __asm__ volatile (
             "lr.w   %0, (%3)\n"      // old = *addr (reservation set)
             "addi   %1, %0, 1\n"     // newval = old + 1
+            "addi   %1, %1, 1\n"      // newval = old + 2
+            "addi   %1, %1, -1\n"     // newval = old + 1
+            "addi   %1, %1, 1\n"      // newval = old + 2
+            "addi   %1, %1, -1\n"     // newval = old + 1
+            "addi   %1, %1, 1\n"      // newval = old + 2
+            "addi   %1, %1, -1\n"     // newval = old + 1
+            "addi   %1, %1, 1\n"      // newval = old + 2
+            "addi   %1, %1, -1\n"     // newval = old + 1
+            "addi   %1, %1, 1\n"      // newval = old + 2
+            "addi   %1, %1, -1\n"     // newval = old + 1
+            "addi   %1, %1, 1\n"      // newval = old + 2
+            "addi   %1, %1, -1\n"     // newval = old + 1
+            "addi   %1, %1, 1\n"      // newval = old + 2
+            "addi   %1, %1, -1\n"     // newval = old + 1
+            "addi   %1, %1, 1\n"      // newval = old + 2
+            "addi   %1, %1, -1\n"     // newval = old + 1
             "sc.w   %2, %1, (%3)\n"  // status = store-conditional
             : "=&r"(old), "=&r"(newval), "=&r"(status)
             : "r"(addr)
             : "memory"
         );
     } while (status != 0);           // retry if SC failed
+
+    return newval;                   // return incremented value
+}
+
+static inline int atomic_inc_amo(int *addr) {
+    int old, newval, status;
+
+    __asm__ volatile (
+        "addi   %2, x0, 1\n"      // %1 = 1
+        "amoadd.w.aqrl  %1, %2, (%3)\n" // %1 = atomic_add(%3, %1)
+        "amoadd.w.aqrl  %1, %2, (%3)\n" // %1 = atomic_add(%3, %1)
+        "amoadd.w.aqrl  %1, %2, (%3)\n" // %1 = atomic_add(%3, %1)
+        "amoadd.w.aqrl  %1, %2, (%3)\n" // %1 = atomic_add(%3, %1)
+        "amoadd.w.aqrl  %1, %2, (%3)\n" // %1 = atomic_add(%3, %1)
+        "amoadd.w.aqrl  %1, %2, (%3)\n" // %1 = atomic_add(%3, %1)
+        "amoadd.w.aqrl  %1, %2, (%3)\n" // %1 = atomic_add(%3, %1)
+        "amoadd.w.aqrl  %1, %2, (%3)\n" // %1 = atomic_add(%3, %1)
+        "amoadd.w.aqrl  %1, %2, (%3)\n" // %1 = atomic_add(%3, %1)
+        "amoadd.w.aqrl  %1, %2, (%3)\n" // %1 = atomic_add(%3, %1)
+        "amoadd.w.aqrl  %1, %2, (%3)\n" // %1 = atomic_add(%3, %1)
+        "amoadd.w.aqrl  %1, %2, (%3)\n" // %1 = atomic_add(%3, %1)
+        "amoadd.w.aqrl  %1, %2, (%3)\n" // %1 = atomic_add(%3, %1)
+        "amoadd.w.aqrl  %1, %2, (%3)\n" // %1 = atomic_add(%3, %1)
+        "amoadd.w.aqrl  %1, %2, (%3)\n" // %1 = atomic_add(%3, %1)
+        "amoadd.w.aqrl  %1, %2, (%3)\n" // %1 = atomic_add(%3, %1)
+        "amoadd.w.aqrl  %1, %2, (%3)\n" // %1 = atomic_add(%3, %1)
+        "amoadd.w.aqrl  %1, %2, (%3)\n" // %1 = atomic_add(%3, %1)
+        "amoadd.w.aqrl  %1, %2, (%3)\n" // %1 = atomic_add(%3, %1)
+        "amoadd.w.aqrl  %1, %2, (%3)\n" // %1 = atomic_add(%3, %1)
+        "amoadd.w.aqrl  %1, %2, (%3)\n" // %1 = atomic_add(%3, %1)
+        : "=&r"(old), "=&r"(newval), "=&r"(status)
+        : "r"(addr)
+        : "memory"
+    );
+
+    if (status == 0) {
+        printf("ERROR, LR/SC should have failed\n");
+    }
 
     return newval;                   // return incremented value
 }
@@ -35,6 +89,27 @@ int main(int argc, char *arg[]) {
 
   barrier(NUM_HARTS);
 
-  printf("%d: LR/SC final value: %d!\n", get_hart_id(), __sync_load(&shared_value));
+  printf("%d: LR/SC int value: %d!\n", get_hart_id(), __sync_load(&shared_value));
+
+  if (get_hart_id() == 0) {
+    for (int i=0; i<NUM_INCREASE; i++) {
+      atomic_inc(&shared_value);
+    }
+  } else {
+    for (int i=0; i<NUM_INCREASE; i++) {
+      atomic_inc_amo(&shared_value);
+    }
+  }
+
+  barrier(NUM_HARTS);
+  printf("%d: LR/SC mix amoadd final value: %d!\n", get_hart_id(), __sync_load(&shared_value));
+
+  for (int i=0; i<NUM_INCREASE; i++) {
+    atomic_inc_amo(&shared_value);
+  }
+
+  barrier(NUM_HARTS);
+
+  printf("%d: Amoadd final value: %d!\n", get_hart_id(), __sync_load(&shared_value));
   return 0;
 }
